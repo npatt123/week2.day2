e7f3192fcd804d4310b324fb83b6ed44133f2cad
npatt123 <107314519+npatt123@users.noreply.github.com>
2022-06-22 21:15:46 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,74 @@
+package week2.Day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead {
+	
+	
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://leaftaps.com/opentaps/control/login");
+	WebElement userName = driver.findElement(By.id("username"));
+	userName.sendKeys("Demosalesmanager");
+	WebElement password = driver.findElement(By.id("password"));
+	password.sendKeys("crmsfa");
+	WebElement clickButton = driver.findElement(By.className("decorativeSubmit"));
+	clickButton.click();	
+	WebElement clickLeads = driver.findElement(By.linkText("CRM/SFA"));
+	clickLeads.click();
+	WebElement tabElement = driver.findElement(By.linkText("Leads"));
+	tabElement.click();	
+	WebElement formElement = driver.findElement(By.linkText("Create Lead"));
+	formElement.click();	
+	WebElement companytnameElement = driver.findElement(By.id("createLeadForm_companyName"));
+	companytnameElement.sendKeys("abco");	
+	WebElement firstnameElement = driver.findElement(By.id("createLeadForm_firstName"));
+	firstnameElement.sendKeys("Nalini");	
+	WebElement lastnameElement = driver.findElement(By.id("createLeadForm_lastName"));
+	lastnameElement.sendKeys("Pattabiram");	
+	WebElement sourceDropDown = driver.findElement(By.id("createLeadForm_dataSourceId"));
+	sourceDropDown.sendKeys("Tradeshow");
+	WebElement firstNameLocalElement = driver.findElement(By.id("createLeadForm_firstNameLocal"));
+	firstNameLocalElement.sendKeys("NaliniP");
+	WebElement departmentElement = driver.findElement(By.name("departmentName"));
+	departmentElement.sendKeys("sociology");
+	WebElement descriptionElement = driver.findElement(By.name("description"));
+	descriptionElement.sendKeys("Hello World");
+	WebElement emailIdEleement = driver.findElement(By.id("createLeadForm_primaryEmail"));
+	emailIdEleement.sendKeys("nalini.pattabiram@gmail.com");
+	
+	WebElement stateDropDown = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+	Select stategdd = new Select(stateDropDown);
+	stategdd.selectByIndex(16);
+	
+	WebElement submitElement =  driver.findElement(By.name("submitButton"));
+	submitElement.click();
+	
+	System.out.println("The Tile is: " + driver.getTitle());
+	driver.close();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
